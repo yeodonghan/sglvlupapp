3f49297540f8da3344baf78dca23b6f24f51a4d8
unknown <yeodonghan@gmail.com>
2021-05-31 12:39:12 +0800

    Fixed subcat loading

--- lib/sub_category/sub_category_bubble.dart
+++ lib/sub_category/sub_category_bubble.dart
@@ -1,7 +1,11 @@
 import 'package:SGLvlUp/audio/SoundsHandler.dart';
 import 'package:SGLvlUp/quiz/quiz_layout.dart';
 import 'package:SGLvlUp/shared/UserProfile.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:asset_cache/asset_cache.dart';
+
+final imageCache = ImageAssetCache(basePath: "assets/levels/");
 
 class SubCategoryBubble extends StatefulWidget {
   String categoryName;
@@ -10,6 +14,16 @@ class SubCategoryBubble extends StatefulWidget {
   int level;
   int maxLevel;
   UserProfile user;
+  String lvl1 = "1VqUXcPuw7j7b0FcUf3uujj3-bnSoToHO";
+  String lvl2 = "1NO97chW6dVRs-ditTZY2h25CLN43mi5d";
+  String lvl3 = "1ZNkq69j8zxn1MhYhnKDztL3QSgYxHi0T";
+  String lvl4 = "1yNU77KHDAt31_EAo54St63GNzT_mJOpO";
+  String lvl5 = "1-k4BMAOfb1qCsPdLFchIch2IT8uVdqeF";
+  String lvl6 = "1JirD-3IAj8RJv_Ps_MrVaoF3UOTLd1bo";
+  String lvl7 = "1lx_fRw06i7R8XoQIdoaCSpiwxEb3AEim";
+  String lvl8 = "1Z6DaoFmMyRlAZP_BbcAfMYI46GIjgLoq";
+  String lvl9 = "1Bv9L81oNEi0x2-OG9OnM9ptsF8_App4D";
+
 
   SubCategoryBubble(
       {Key key,
@@ -29,14 +43,56 @@ class SubCategoryBubble extends StatefulWidget {
     level = this.level;
     maxLevel = this.maxLevel;
     user = this.user;
+
     return _SubCategoryBubbleState();
   }
 }
 
 class _SubCategoryBubbleState extends State<SubCategoryBubble> {
+  String levelUrl;
+
+
   @override
   void initState() {
     print('This state is of Level: ' + widget.level.toString());
+    switch(widget.level) {
+      case 1: {
+        levelUrl = widget.lvl1;
+      }
+      break;
+      case 2: {
+        levelUrl = widget.lvl2;
+      }
+      break;
+      case 3: {
+        levelUrl = widget.lvl3;
+      }
+      break;
+      case 4: {
+        levelUrl = widget.lvl4;
+      }
+      break;
+      case 5: {
+        levelUrl = widget.lvl5;
+      }
+      break;
+      case 6: {
+        levelUrl = widget.lvl6;
+      }
+      break;
+      case 7: {
+        levelUrl = widget.lvl7;
+      }
+      break;
+      case 8: {
+        levelUrl = widget.lvl8;
+      }
+      break;
+      default: {
+        levelUrl = widget.lvl9;
+      }
+      break;
+    }
     super.initState();
   }
 
@@ -74,18 +130,20 @@ class _SubCategoryBubbleState extends State<SubCategoryBubble> {
                         widget.user)),
               );
             },
+
             child: Container(
               width: MediaQuery.of(context).size.height * 0.07,
               height: MediaQuery.of(context).size.height * 0.07,
               decoration: BoxDecoration(
                 shape: BoxShape.circle,
                 image: DecorationImage(
-                    image: AssetImage('assets/levels/level' +
-                        widget.level.toString() +
-                        '.jpg'),
+                    image: CachedNetworkImageProvider(
+                        "http://drive.google.com/uc?export=view&id=$levelUrl"),
                     fit: BoxFit.cover),
               ),
             ),
+
+
           ),
           SizedBox(height: MediaQuery.of(context).size.height * 0.01),
           Container(
@@ -103,99 +161,3 @@ class _SubCategoryBubbleState extends State<SubCategoryBubble> {
     );
   }
 }
-
-/*class SubCategoryBubble extends StatefulWidget {
-  String categoryName;
-  int categoryID;
-  String quizName;
-  int level;
-  int maxLevel;
-  UserProfile user;
-
-  SubCategoryBubble(
-      {Key key,
-      this.categoryName,
-      this.categoryID,
-      this.quizName,
-      this.level,
-      this.maxLevel,
-      this.user})
-      : super(key: key);
-
-  @override
-  State<StatefulWidget> createState() {
-    categoryName = this.categoryName;
-    categoryID = this.categoryID;
-    quizName = this.quizName;
-    level = this.level;
-    maxLevel = this.maxLevel;
-    user = this.user;
-    return _SubCategoryBubble();
-  }
-}
-
-//test
-
-class SubCategoryBubbleState extends State<SubCategoryBubble> {
-  @override
-  void initState() {
-    print('This state is of Level: ' + widget.level.toString());
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 120,
-      width: 75,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Material(
-            color: Colors.white,
-            child: InkWell(
-              onTap: () {
-                */ /*SoundsHandler().playTap();
-                print(widget.categoryName);
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => QuizLayout(
-                          widget.categoryName,
-                          widget.categoryID,
-                          widget.quizName,
-                          level,
-                          maxLevel,
-                          widget.user)),
-                );*/ /*
-              },
-              child: Container(
-                width: MediaQuery.of(context).size.height * 0.07,
-                height: MediaQuery.of(context).size.height * 0.07,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  image: DecorationImage(
-                      image: AssetImage(
-                          'assets/levels/level' + level.toString() + '.jpg'),
-                      fit: BoxFit.cover),
-                ),
-              ),
-            ),
-          ),
-          SizedBox(height: MediaQuery.of(context).size.height * 0.01),
-          Container(
-            color: Colors.white,
-            width: MediaQuery.of(context).size.height * 0.07,
-            height: MediaQuery.of(context).size.height * 0.035,
-            child: Text(
-              widget.quizName,
-              style: TextStyle(fontSize: 10),
-              textAlign: TextAlign.center,
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}*/
